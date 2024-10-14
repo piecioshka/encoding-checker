2258ed3fa97b720bb9c27cfb569c80d4d9eea322
Piotr Kowalski <piecioshka@gmail.com>
2024-10-14 21:40:21 +0200

    Fix unit tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,36 +1,34 @@
-'use strict';
+"use strict";
 
-const verify = require('./index').verify;
-const glob = require('glob-promise');
+const verify = require("./index").verify;
+const glob = require("glob-promise");
 
-describe('General', () => {
-    it('should works with single file', () => {
-        return verify('utf-8', [__filename])
-            .then((result) => {
-                expect(result).toEqual(jasmine.any(Array));
-                expect(result.length).toEqual(1);
-                expect(result[0].encoding).toEqual('ascii');
-                expect(result[0].file).toEqual(__filename);
-            });
+describe("General", () => {
+    it("should works with single file", () => {
+        return verify("utf-8", [__filename]).then((result) => {
+            expect(result).toEqual(jasmine.any(Array));
+            expect(result.length).toEqual(1);
+            expect(result[0].encoding).toEqual("ascii");
+            expect(result[0].file).toEqual(__filename);
+        });
     });
 
-    it('should works with many files', () => {
-        return glob('*')
-            .then((files) => verify('utf-8', files))
+    it("should works with many files", () => {
+        return glob("*")
+            .then((files) => verify("utf-8", files))
             .then((result) => {
                 expect(result).toEqual(jasmine.any(Array));
-                expect(result.length).toEqual(5);
+                expect(result.length).toEqual(4);
                 result.forEach(({ encoding }) => {
-                    expect(encoding).toEqual('ascii');
-                })
+                    expect(encoding).toEqual("ascii");
+                });
             });
     });
 
-    it('should fail when put not valid params', () => {
-        return verify('utf-8', ['.'])
-            .then((result) => {
-                expect(result).toEqual(jasmine.any(Array));
-                expect(result.length).toEqual(0);
-            });
+    it("should fail when put not valid params", () => {
+        return verify("utf-8", ["."]).then((result) => {
+            expect(result).toEqual(jasmine.any(Array));
+            expect(result.length).toEqual(0);
+        });
     });
 });
